f64c6bc12d4b9d7ef5504b845878a72bf775c62e
Ella711 <allenro711@gmail.com>
2022-07-29 11:23:22 -0500

    ADD - File for task16

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -7,9 +7,8 @@ Function that adds 2 matrices
 def add_matrices(mat1, mat2):
     """ Returns the addition of two matrices """
     import numpy as np
-    mat1copy = np.array([row[:] for row in mat1])
-    mat2copy = np.array([row[:] for row in mat2])
-    if mat1copy.shape != mat2copy.shape:
+    mat1, mat2 = np.array(mat1), np.array(mat2)
+    if mat1.shape != mat2.shape:
         return None
-    result = mat1copy + mat2copy
+    result = mat1 + mat2
     return result
