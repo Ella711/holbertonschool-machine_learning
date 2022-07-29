95ffb981f4a12a540986194078720620d56c4c3f
Ella711 <allenro711@gmail.com>
2022-07-29 10:41:21 -0500

    ADD - File for task15

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Function that slices a matrix along specific axes
+- axes is a dictionary where the key is an axis to slice along
+    and the value is a tuple representing the slice to make along that axis
+- You can assume that axes represents a valid slice
+"""
+
+
+def np_slice(matrix, axes={}):
+    """ slice matrix based on axis given"""
+    slices = []
+    for i in range(matrix.ndim):
+        values = axes.get(i)
+        if values is not None:
+            slices.append(slice(*values))
+        else:
+            slices.append(slice(None, None, None))
+    return matrix[slices]
