934335f25600aedc6cb822cb8427349d5d17e4f7
Ella711 <allenro711@gmail.com>
2022-08-03 16:28:50 -0500

    ADD - File for task10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -7,9 +7,10 @@ Function that calculates the derivative of a polynomial
 def poly_derivative(poly):
     """ Returns the derivative of a polynomial """
     derivative = []
-    if poly:
+    if type(poly) == list and len(poly) >= 1:
         if len(poly) == 1:
             return [0]
-    for i in range(1, len(poly)):
-        derivative.append(i * poly[i])
-    return derivative
+        for i in range(1, len(poly)):
+            derivative.append(i * poly[i])
+        return derivative
+    return None
