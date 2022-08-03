fbc761b3b60c947a60985cfa68ea055f1f5bf52a
Ella711 <allenro711@gmail.com>
2022-08-03 17:05:19 -0500

    ADD - File for task17

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,8 +6,8 @@ Function that calculates the integral of a polynomial
 
 def poly_integral(poly, C=0):
     """ Returns the integral of a polynomial """
-    integral = [C]
-    if type(poly) == list and len(poly) >= 1:
+    if type(poly) == list and len(poly) >= 1 and type(C) == int or float:
+        integral = [C]
         for i in range(1, len(poly) + 1):
             integ = poly[i - 1] / i
             if (integ - int(integ)) == 0:
