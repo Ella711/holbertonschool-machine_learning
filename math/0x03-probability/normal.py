b9c2a413df7f6348e1fa0d09020aa2a797a07f83
Ella711 <allenro711@gmail.com>
2022-08-09 18:01:55 -0500

    ADD - File for task6

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Contains the class Normal that represents a normal distribution
+"""
+
+
+class Normal:
+    """ Represents a normal distribution """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Class constructor
+
+        data is a list of the data to be used to estimate the distribution
+        mean is the mean of the distribution
+        stddev is the standard deviation of the distribution
+        """
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            length = len(data)
+            self.mean = sum(data) / length
+            self.stddev = (sum(map(lambda n: pow(n - self.mean, 2),
+                                   data)) / length) ** .5
+        else:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
