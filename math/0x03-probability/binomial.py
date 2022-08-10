78f3dcc27d48769215249424039150de3aa96360
Ella711 <allenro711@gmail.com>
2022-08-10 11:30:15 -0500

    ADD - File for task10

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -30,5 +30,5 @@ class Binomial:
                 raise ValueError("n must be a positive value")
             if p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
-            self.n = n
+            self.n = int(n)
             self.p = float(p)
