5a2835787495b273b8721691dee5ff7f4d682235
Ella711 <allenro711@gmail.com>
2022-08-21 16:14:47 -0500

    [ADD] - File for task17

---
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Module for class DeepNeuralNetwork
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    Defines a deep neural network performing
+    binary classification
+    """
+
+    def __init__(self, nx, layers):
+        """
+        Class Constructor
+
+        nx is the number of input features
+        layers is a list representing the number of
+            nodes in each layer of the network
+        """
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or layers == []:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for layer in range(self.__L):
+            if layers[layer] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            if layer == 0:
+                layer_prev = nx
+            else:
+                layer_prev = layers[layer - 1]
+            self.__weights['W' + str(layer + 1)] = np.random.randn(
+                layers[layer], layer_prev) * np.sqrt(2 / layer_prev)
+            self.__weights['b' + str(layer + 1)] = np.zeros((layers[layer], 1))
+
+    @property
+    def L(self):
+        """ Getter for L """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Getter for cache """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ Getter for weights """
+        return self.__weights
