1d27b96958b83e3422dce3cc84243916b5897f99
Ella711 <allenro711@gmail.com>
2022-08-22 15:38:09 -0500

    [ADD] - File for task24

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -7,6 +7,8 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """ Converts a numeric label vector into a one-hot matrix """
+    if type(Y) != np.ndarray:
+        return None
     try:
         one_hot = np.eye(classes)[Y].T
         return one_hot
