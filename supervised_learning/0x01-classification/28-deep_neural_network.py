a94f220868b9a0069c96df50e6a5ab07447ff3ba
Ella711 <allenro711@gmail.com>
2022-08-23 16:19:11 -0500

    [ADD] - File for task28

--- supervised_learning/0x01-classification/28-deep_neural_network.py
+++ supervised_learning/0x01-classification/28-deep_neural_network.py
@@ -78,7 +78,7 @@ class DeepNeuralNetwork:
                 A = self.softmax(Z)
             elif self.__activation == "tanh":
                 A = self.tanh(Z)
-            else:
+            elif self.__activation = "sig":
                 A = self.sigmoid(Z)
             self.__cache["A" + str(layer)] = A
         return A, self.__cache
@@ -107,7 +107,7 @@ class DeepNeuralNetwork:
                 dz = A - Y
             elif self.__activation == "tanh":
                 dz = da * (1 - (A ** 2))
-            else:
+            elif self.__activation == "sig":
                 dz = da * (A * (1 - A))
             dw = np.matmul(dz, A_prev.T) / m
             db = np.sum(dz, axis=1, keepdims=True) / m
