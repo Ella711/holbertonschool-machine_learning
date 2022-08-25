a09bf44999706581493ea12922fb05ea82be4e11
Ella711 <allenro711@gmail.com>
2022-08-25 13:47:03 -0500

    [ADD] - File for task1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -8,5 +8,8 @@ import tensorflow.compat.v1 as tf
 def create_layer(prev, n, activation):
     """ Creates a layer """
     heetal = tf.keras.initializers.VarianceScaling(mode='fan_avg')
-    L = tf.keras.layers.Dense(n, activation=activation, name="layer", kernel_initializer=heetal)(prev)
+    L = tf.keras.layers.Dense(n,
+                              activation=activation,
+                              name="layer",
+                              kernel_initializer=heetal)(prev)
     return L
