f307973a73307ac3224ca6376998452ca200c995
Ella711 <allenro711@gmail.com>
2022-08-31 17:30:31 -0500

    [FIX] - spacing

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -21,21 +21,21 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
     save_path: path to where the model should be saved after training
     Returns: the path where the model was saved
     """
-    with tf.Session() as sess:  # import meta graph and restore session
+    with tf.Session() as sess:
         saver = tf.train.import_meta_graph(load_path + ".meta")
         saver.restore(sess, load_path)
-        x = tf.get_collection("x")[0]  # load tensors and ops
+        x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
         train_op = tf.get_collection("train_op")[0]
         feed_train = {x: X_train, y: Y_train}
         feed_valid = {x: X_valid, y: Y_valid}
-        m = X_train.shape[0]  # Form batch sizes
+        m = X_train.shape[0]
         batches = m // batch_size
         if m % batch_size != 0:
             batches += 1
-        for i in range(epochs + 1):  # loop over epochs
+        for i in range(epochs + 1):
             train_cost, train_acc = sess.run([loss, accuracy], feed_train)
             valid_cost, valid_acc = sess.run([loss, accuracy], feed_valid)
             print("After {} epochs:".format(i))
@@ -52,7 +52,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
                     x_batch = x_shuffle[start:end]
                     y_batch = y_shuffle[start:end]
                     feed_batch = {x: x_batch, y: y_batch}
-                    sess.run(train_op, feed_batch)  # train
+                    sess.run(train_op, feed_batch)
                     if step % 100 == 0:
                         cost, acc = sess.run([loss, accuracy], feed_batch)
                         print("\tStep {}:".format(step))
