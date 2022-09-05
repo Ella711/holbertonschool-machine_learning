ca4403a7801f8976017f7c839124e5fd422a331a
Ella711 <allenro711@gmail.com>
2022-09-05 15:35:33 -0500

    [ADD] - File for task2

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -15,6 +15,6 @@ def precision(confusion):
     Returns: np.ndarray - shape (classes,) - contains sensitivity of each class
     """
     correct_predicted = np.sum(confusion, axis=0)
-    total_predicted = np.diagonal(confusion)
-    precision = total_predicted / correct_predicted
+    predicted_labels = np.diagonal(confusion)
+    precision = predicted_labels / correct_predicted
     return precision
