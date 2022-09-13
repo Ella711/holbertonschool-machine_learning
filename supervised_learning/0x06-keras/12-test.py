a1d42373153f9143150e23a508d05f2d0cea194f
Ella711 <allenro711@gmail.com>
2022-09-13 16:02:24 -0500

    [ADD] - File for task12

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+12. Test
+"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    Tests a neural network
+
+    network: model to test
+    data: input data to test the model with
+    labels: correct one-hot labels of data
+    verbose: boolean determines if output should be printed during process
+
+    Returns: the loss and accuracy of the model with the
+        testing data, respectively
+    """
+    loss, acc = network.evaluate(
+        x=data, y=labels, verbose=verbose
+    )
+    return loss, acc
