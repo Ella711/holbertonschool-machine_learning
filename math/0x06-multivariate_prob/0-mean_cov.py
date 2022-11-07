27f85db541e939b4187f74e3dd4c439c921b8dcd
Ella711 <allenro711@gmail.com>
2022-11-07 18:43:12 -0500

    [ADD] - File for task0

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -20,6 +20,7 @@ def mean_cov(X):
     if n < 2:
         raise ValueError("X must contain multiple data points")
 
-    mean = np.mean(X, axis=0, keepdims=True)
-    cov = np.matmul((X - mean).T, (X - mean)) / (n - 1)
-    return mean, cov
+    mean = X.sum(axis=0) / n
+    deviation = X - mean
+    covariant = np.matmul(deviation.T, deviation)
+    return mean[np.newaxis, ...], covariant / (n - 1)
