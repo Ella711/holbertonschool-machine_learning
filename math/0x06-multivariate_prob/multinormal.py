17983fc7e84e20e5f846176c7046639a12c22986
Ella711 <allenro711@gmail.com>
2022-11-07 20:02:53 -0500

    [ADD] - File for task3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -35,3 +35,24 @@ class MultiNormal:
         deviation = X - mean
         covariant = np.matmul(deviation.T, deviation)
         return mean.reshape((d, 1)), covariant / (n - 1)
+
+    def pdf(self, x):
+        """
+        Calculates the PDF at a data point
+        Args:
+            x: np.ndarray - shape (d, 1) containing the data points
+
+        Returns: value of the PDF
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        m = self.mean
+        cov = self.cov
+        denominator = np.sqrt(((2 * np.pi) ** d) * (np.linalg.det(cov)))
+        inverse = np.linalg.inv(cov)
+        exp = (-.5 * np.matmul(np.matmul((x - m).T, inverse), (x - m)))
+        pdf = (1 / denominator) * np.exp(exp[0][0])
+        return pdf
