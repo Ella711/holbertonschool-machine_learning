3e5f2e513b7c3f85134886e15496c7e8f7aa8538
Ella711 <allenro711@gmail.com>
2022-11-07 19:32:53 -0500

    [ADD] - File for task2

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -19,8 +19,6 @@ class MultiNormal:
         n, _ = data.shape
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        if n < 2:
-            raise ValueError("data must contain multiple data points")
 
         self.mean, self.cov = self.mean_cov(data.T)
 
@@ -29,7 +27,12 @@ class MultiNormal:
         """
         Calculates the mean and covariance of a data set
         """
+        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
         n, d = X.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+
         mean = np.mean(X, axis=0, keepdims=True)
         deviation = X - mean
         covariant = np.matmul(deviation.T, deviation)
