47788c7ea9b7a4714469ab97a703fa7a31d3b818
Ella711 <allenro711@gmail.com>
2022-11-10 12:19:04 -0500

    [ADD] - File for task0

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -21,16 +21,17 @@ def likelihood(x, n, P):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than "
-                         "or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) and len(P.shape) == 1:
+    if not (isinstance(P, np.ndarray) and len(P.shape) == 1):
         raise TypeError("P must be a 1D numpy.ndarray")
     if any([True if (x > 1 or x < 0) else False for x in P]):
         raise ValueError("All values in P must be in the range [0, 1]")
-
+    # Binomal distribution likelihood
+    # pr(x|n, p) = (n! / x!(n - x)!)* (p**x) * (1 - p)**(n - x)
     factorial = np.math.factorial
-    llhood = (factorial(n) / (factorial(x) * factorial(n - x)) * (P ** x)
-              * ((1 - P) ** (n - x)))
+    llhood = (factorial(n) / (factorial(x) * factorial(n - x)) * (P ** x) *
+              ((1 - P) ** (n - x)))
     return llhood
