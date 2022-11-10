326f62e710107b16f73fba94d8cdc9e58aac4ef4
Ella711 <allenro711@gmail.com>
2022-11-10 12:49:47 -0500

    [ADD] - File for task3

--- math/0x07-bayesian_prob/3-posterior.py
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -31,12 +31,11 @@ def posterior(x, n, P, Pr):
         raise TypeError("P must be a 1D numpy.ndarray")
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    for i in range(P.shape[0]):
-        if P[i] > 1 or P[i] < 0:
-            raise ValueError("All values in P must be in the range [0, 1]")
-        if Pr[i] > 1 or Pr[i] < 0:
-            raise ValueError("All values in Pr must be in the range [0, 1]")
-    if np.isclose([np.sum(Pr)], [1]) == [False]:
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError('All values in P must be in the range [0, 1]')
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
+    if np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
 
     factorial = np.math.factorial
