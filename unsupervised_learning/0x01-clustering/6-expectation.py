5dc4b642d088dc29359af5383c14e1fd15b19723
Ella711 <allenro711@gmail.com>
2022-11-15 10:46:52 -0500

    [ADD] - File for task6

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -21,23 +21,27 @@ def expectation(X, pi, m, S):
 
     Returns: g, l, or None, None on failure
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or not isinstance(pi, np.ndarray)\
+        or not isinstance(m, np.ndarray) or not isinstance(S, np.ndarray)\
+        or X.ndim != 2 or pi.ndim != 1 or m.ndim != 2 or S.ndim != 3\
+        or pi.shape[0] != m.shape[0] or pi.shape[0] != S.shape[0]\
+        or X.shape[1] != m.shape[1] or X.shape[1] != S.shape[1]\
+        or S.shape[1] != S.shape[2] or np.any(np.linalg.det(S) == 0)\
+            or not np.isclose(pi.sum(), 1):
         return None, None
-    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
-        return None, None
-    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
-        return None, None
-    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
-        return None, None
-    if not np.isclose([np.sum(pi)], [1])[0]:
-        return None, None
-    pdfs = np.ndarray((m.shape[0], X.shape[0]))
 
-    for cluster in range(m.shape[0]):
-        pdfs[cluster] = pdf(X, m[cluster], S[cluster])
+    k = pi.shape[0]
+    n, d = X.shape
+    g = np.empty((k, n))
 
-    pdfs = pdfs * pi[:, np.newaxis]
-    pdfsum = pdfs.sum(axis=0)
-    expects = pdfs / pdfsum
+    for i in range(k):
+        likelihood = pdf(X, m[i], S[i])
+        prior = pi[i]  # (1,)
+        intersection = prior * likelihood
+        g[i] = intersection
 
-    return expects, np.log(pdfsum).sum()
+    marginal = np.sum(g, axis=0, keepdims=True)
+    g /= marginal
+
+    log = np.sum(np.log(np.sum(marginal, axis=0)), axis=0)
+    return g, log
