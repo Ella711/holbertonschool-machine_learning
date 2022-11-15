4d28da97b1ec578bcf74aeb8352a28765fdbe825
Ella711 <allenro711@gmail.com>
2022-11-15 10:49:05 -0500

    [ADD] - File for task3

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -21,24 +21,27 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
 
     Returns: results, d_vars, or None, None on failure
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None
-    if iterations <= 0 or not isinstance(iterations, int):
-        return None, None
-    if kmax is not None and not isinstance(kmax, int) or kmax <= 0:
-        return None, None
-    if not kmax:
-        kmax = X.shape[0]
-    if not isinstance(kmin, int) or kmin >= kmax or kmin <= 0:
-        return None, None
+    try:
+        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+            return None, None
+        if iterations <= 0 or not isinstance(iterations, int):
+            return None, None
+        if kmax is not None and not isinstance(kmax, int) or kmax <= 0:
+            return None, None
+        if not kmax:
+            kmax = X.shape[0]
+        if not isinstance(kmin, int) or kmin >= kmax or kmin <= 0:
+            return None, None
 
-    results, d_vars = [], []
+        results, d_vars = [], []
 
-    for k in range(kmin, kmax + 1):
-        C, clss = kmeans(X, k, iterations)
-        results.append((C, clss))
-        d_vars.append(variance(X, C))
+        for k in range(kmin, kmax + 1):
+            C, clss = kmeans(X, k, iterations)
+            results.append((C, clss))
+            d_vars.append(variance(X, C))
 
-    d_vars = [d_vars[0] - x for x in d_vars]
+        d_vars = [d_vars[0] - x for x in d_vars]
 
-    return results, d_vars
+        return results, d_vars
+    except Exception:
+        return None, None
