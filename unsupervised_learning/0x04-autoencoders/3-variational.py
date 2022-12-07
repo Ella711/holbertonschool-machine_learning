fe848f3959a88fb49bf3f690cd962ee6a12a64fb
Ella711 <allenro711@gmail.com>
2022-12-07 17:14:01 -0500

    [ADD] - File for task3

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -18,43 +18,46 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     Returns: encoder, decoder, auto
     """
     k = keras.layers
+    K = keras.backend
+    Loss = keras.losses.binary_crossentropy
 
     def sampling(args):
-        mean, log = args
-        epsilon = keras.backend.random_normal(
-            shape=(keras.backend.shape(mean)[0], latent_dims),
-            mean=0,
-            stddev=1
-        )
-        return mean + keras.backend.exp(log / 2) * epsilon
+        """Samples similar points from latent space"""
+        mean, log_sigma = args
+        epsilon = K.random_normal(
+            shape=(K.shape(mean)[0], latent_dims), mean=0, stddev=0.1)
+        return mean + K.exp(log_sigma) * epsilon
 
-    input = keras.Input(shape=input_dims)
-    encode = k.Dense(hidden_layers[0], activation='relu')(input)
-    for layer in hidden_layers[1:]:
-        encode = k.Dense(layer, activation='relu')(encode)
-    encode_mean = k.Dense(latent_dims)(encode)
-    encode_log = k.Dense(latent_dims)(encode)
-    encoding = k.Lambda(sampling)([encode_mean, encode_log])
-    encoder = keras.Model(input, [encode_mean, encode_log, encoding])
-
+    input_layer = keras.Input(shape=input_dims)
     coded_input = keras.Input(shape=latent_dims)
-    decode = k.Dense(hidden_layers[-1], activation='relu')(coded_input)
-    for layer in hidden_layers[-2::-1]:
-        decode = k.Dense(layer, activation='relu')(decode)
-    decode = k.Dense(input_dims, activation='sigmoid')(decode)
-    decoder = keras.Model(coded_input, decode)
 
-    auto = keras.Model(input, decoder(encoder(input)[-1]))
+    encoded_layers = k.Dense(hidden_layers[0], activation="relu")(input_layer)
 
-    def vae_loss(inputs, outputs):
-        r_loss = keras.losses.binary_crossentropy(inputs, outputs)
-        r_loss *= input_dims
-        k_exp = keras.backend.exp(encode_log)
-        k_square = keras.backend.square(encode_mean)
-        lat_loss = -0.5 * keras.backend.sum(1 + encode_log - k_exp
-                                            - k_square, axis=-1)
-        return keras.backend.mean(lat_loss + r_loss)
+    for nodes in hidden_layers[1:]:
+        encoded_layers = k.Dense(nodes, activation="relu")(encoded_layers)
 
-    auto.compile(loss=vae_loss, optimizer='adam')
+    encoded_layers = k.Dense(latent_dims)(encoded_layers)
+    encode_mean = k.Dense(latent_dims)(encoded_layers)
+    encode_log = k.Dense(latent_dims)(encoded_layers)
+    Z = k.Lambda(sampling)([encode_mean, encode_log])
 
-    return encoder, decoder, auto
+    decoded = k.Dense(hidden_layers[-1], activation="relu")(coded_input)
+    for dim in hidden_layers[-2::-1]:
+        decoded = keras.layers.Dense(dim, activation='relu')(decoded)
+    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+
+    encoder = keras.Model(input_layer, [encode_mean, encode_log, Z])
+    decoder = keras.Model(coded_input, decoded)
+    outputs = decoder(encoder(input_layer)[2])
+    autoencoder = keras.Model(input_layer, outputs)
+
+    reconstruction_loss = Loss(input_layer, outputs)
+    reconstruction_loss *= input_dims
+    k1_loss = 1 + encode_log - K.square(encode_mean) - K.exp(encode_log)
+    k1_loss = K.sum(k1_loss, axis=-1)
+    k1_loss *= -0.5
+    vae_loss = K.mean(reconstruction_loss + k1_loss)
+    autoencoder.add_loss(vae_loss)
+    autoencoder.compile(optimizer="adam")
+
+    return encoder, decoder, autoencoder
