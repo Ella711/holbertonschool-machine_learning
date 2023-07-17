98a8e971758d632e0cc353a061c91a145396c5af
Ella711 <allenro711@gmail.com>
2023-07-17 13:18:46 -0500

    [FIX] - logic

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -9,17 +9,18 @@ def sentientPlanets():
     """
     Returns the list of names of the home planets of all sentient species
     """
-    url = "https://swapi-api.hbtn.io/api/species"
-    getreq_species = requests.get(url)
-    planet_list = []
-    while getreq_species.status_code == 200:
-        for species in getreq_species.json()["results"]:
-            url = species["homeworld"]
-            if url is not None:
-                homeworld = requests.get(url)
-                planet_list.append(homeworld.json()["name"])
-        try:
-            getreq_species = requests.get(getreq_species.json()["next"])
-        except Exception:
-            break
-    return planet_list
+    url = "https://swapi-api.hbtn.io/api/species/?format=json"
+    species_list = []
+    while url:
+        species_results = requests.get(url).json()
+        species_list += species_results.get('results')
+        url = species_results.get('next')
+    planets = []
+    for species in species_list:
+        if species.get('designation') == 'sentient' or \
+           species.get('classification') == 'sentient':
+            url = species.get('homeworld')
+            if url:
+                planet = requests.get(url).json()
+                planets.append(planet.get('name'))
+    return planets
