909bf6967668bcb9243239485ebd155aa5a12d10
Ella711 <allenro711@gmail.com>
2023-07-24 18:40:24 -0500

    [ADD] - Readme and files for directory on databases

---
+++ pipeline/0x02-databases/0-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates the database db_0 in your MySQL server.
+-- If the database db_0 already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS db_0;
---
+++ pipeline/0x02-databases/17-store.sql
@@ -0,0 +1,15 @@
+-- Script that creates a trigger that decreases
+-- the quantity of an item after adding a new order.
+DROP TRIGGER IF EXISTS UpdateItems;
+
+DELIMITER $$
+CREATE TRIGGER UpdateItems
+    AFTER INSERT
+    ON 'orders' FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET items.quantity = items.quantity - NEW.number
+	WHERE items.name = NEW.item_name;
+END $$
+
+DELIMITER;
