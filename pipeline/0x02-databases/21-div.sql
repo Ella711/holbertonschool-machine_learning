24419748c6865cd27a0f43bd603d570c99407d30
Ella711 <allenro711@gmail.com>
2023-07-24 19:22:20 -0500

    [ADD] - File for task 21

--- pipeline/0x02-databases/21-div.sql
+++ pipeline/0x02-databases/21-div.sql
@@ -3,12 +3,16 @@
 -- a, INT
 -- b, INT
 -- And returns a / b or 0 if b == 0
+DELIMITER //
+
 CREATE FUNCTION SafeDiv (a INT, b INT)
 RETURNS FLOAT
+DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN (a / b);
-    END IF;
-END
+        IF b = 0 THEN
+	   RETURN 0;
+	ELSE
+		RETURN (a / b);
+	END IF;
+END //
+DELIMITER;
